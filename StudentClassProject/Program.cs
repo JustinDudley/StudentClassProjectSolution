8f8110ddf0fcae998f07f004c8c18c5d9d98492c
Justin Dudley <justindudley99@gmail.com>
2019-08-27 16:34:21 -0400

    Add project files.

---
+++ StudentClassProject/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace StudentClassProject {
+    class Program {
+        static void Main(string[] args) {
+
+            var Lidya = new Student();  //first, identify which Student you're working with. (Lidya.Firstname is diff from Justin.Firstname -- I think that's the point).
+            Lidya.Firstname = "Lidya";
+            Lidya.Lastname = "Tsegay";
+            Lidya.SetFinalGrade(98);
+            var fullname = Lidya.GetFullname();
+            Console.WriteLine("Lidya's full name is " + fullname);
+            // when a method returns something, you can can put that on the right  side of equal
+
+
+            var Justin = new Student();
+            Justin.Firstname = "Justin";
+            Justin.Lastname = "Dudley";
+            Justin.SetFinalGrade(99);
+            fullname = Justin.GetFullname();
+            Console.WriteLine($"{fullname}"); //This is a better way to concatenate. This string is going to do something called interpolation . The $ says:  Interpolation coming. "Don't put the name, put the VALUE"
+            // -because concatenation of a lot of little strings is very error-prone.  Bad.  This is a better way
+            //Console.WriteLine($"")
+
+            var Nick = new Student();
+            Nick.Firstname = "Nick";
+            Nick.Lastname = "Baker";
+            Nick.SetFinalGrade(-100);
+            Console.WriteLine($"{Nick.Firstname} {Nick.Lastname}");
+
+        }
+    }
+}
---
+++ StudentClassProject/Student.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentClassProject {
+    class Student {
+
+        public string Firstname { get; set; }  //get  read data       set change data
+        public string Lastname { get; set; }
+        //every single SS is going to have a first name, and a last name
+        public string Email { get; set; }
+        public string Phone { get; set; }   
+        public int FinalGrade { get; set; }
+        //SHORTCUT:  type "prop" and tab twice, to get template
+
+        // public = access modifier.  what access do you allow?.   "public", "private" "internal (default.)"
+        // from inside the Student class, all things in class can be accessed.  But what can OTHER classes access?
+
+        //public:  ANY other class can have access to this property
+        //private:  NO other class can access this.  ONLY in the class it's defined in
+        //internal: Can be accessed by any class in same project as this one.
+
+
+        //METHODS
+        public string GetFullname() {        //parenths empty.  Don't need to pass anything in       // will return a string
+            return Firstname + " " + Lastname;
+        }
+        public void SetFinalGrade(int finalGrade) {            // void:  It doesn't return anything.  Like a null.  a value that means it has no value
+            if (finalGrade < 0) {    // check for bad input
+                Console.WriteLine("ERROR:  Grade must be greater than zero");
+                return; // so we never get to the part below where the final grade gets changed.
+            }
+            FinalGrade = finalGrade;    // the property starts with a capital F
+                                        // these are two different variables!  "The FfinalGrade [don't know which!!] property is set to that value"
+                                        // OR, could NOT write return, but instead put FinalGrade = finalGrade inside an else clause
+        }
+    }
+}
